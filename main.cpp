923ee20de0a959d25d30168cf7d0177cdf6a930a
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-10-10 16:21:51 -0300

    To the Moon!

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "LibraryCode.hpp"
+
+int main(int argc, char **argv){
+	std::cout << "Actual application code" << std::endl;
+	std::cout << "2 + 3 = " << add(2, 3) << std::endl;
+	return 0;
+}
