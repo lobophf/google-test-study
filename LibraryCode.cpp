339f461e7d829ead44a11450aaec61b5a83df56f
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-10-10 17:51:55 -0300

    unit test basic structure

---
+++ LibraryCode.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include "LibraryCode.hpp"
+
+bool isPositive(int x){
+	return x > 0;
+}
+
+int countPositives(std::vector<int> const& inputVector){
+	return std::count_if(inputVector.begin(), inputVector.end(), isPositive);
+}
